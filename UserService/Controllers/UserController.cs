82342518543a82d334940bbb1030a98e065953c4
RohitGoswami <rohitgoswamigi@gmail.com>
2021-10-19 21:55:35 +0530

    update some files

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UserService.Database;
 using UserService.Database.Entity;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace UserService.Controllers
 {
