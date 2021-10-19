221780c90194ab61e3ae09340c1316ff9768b0f7
RohitGoswami <rohitgoswamigi@gmail.com>
2021-10-19 21:17:30 +0530

    add some file

---
+++ UserService/Database/Entity/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UserService.Database.Entity
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+    }
+}
